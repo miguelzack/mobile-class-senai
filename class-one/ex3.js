0f7e22b1eedfbe5ff2fee86e9cad7cad28c83c6e
miguelzackk <mzsilvaw@gmail.com>
2026-01-29 10:52:54 -0300

    feat: finish ex3

---
+++ class-one/ex3.js
@@ -0,0 +1,8 @@
+function exemploLet() {
+//   console.log(minhaLet); reference error
+  let minhaLet = "Isso é uma let";
+  console.log(minhaLet + " dentro da função");
+}
+exemploLet();
+
+// console.log(minhaLet);
