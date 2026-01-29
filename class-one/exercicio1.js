932847df8def674471ccb6c6678aa1e1d5cf8f2c
miguelzackk <mzsilvaw@gmail.com>
2026-01-29 11:01:35 -0300

    feat: finish exercicio1 and exercicio2

---
+++ class-one/exercicio1.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome:");
+let sobrenome = prompt("Digite seu sobrenome:");
+let idade = prompt("Digite sua idade:");
+console.log(
+  "Seu nome é " + nome + " " + sobrenome + " e a sua idade é " + idade + ".",
+);
---
+++ class-one/exercicio2.js
@@ -0,0 +1,9 @@
+function Exercicio2() {
+  if (true) {
+    let minhaLet = "Programando em JavaScript";
+    console.log(minhaLet);
+  } else {
+    console.log("Operação falsa");
+  }
+  console.log(minhaLet);
+}
