9d4d84e38fe99d3cd6b842e5f47efbf258723b2f
miguelzackk <mzsilvaw@gmail.com>
2026-02-05 09:01:43 -0300

    started class-two

---
+++ class-two/js/index.js
@@ -0,0 +1,22 @@
+const exemploArrayLiteralDiv = document.getElementById('exemplo-array-literal')
+
+const frutas = ["maçã", "uva", "banana"]
+
+exemploArrayLiteralDiv.innerHTML = `
+<h2>Exemplo de array com literal</h2>
+<p>Array: [${frutas}]</p>
+`
+
+
+const exemploArrayAcessoDiv = document.getElementById('exemplo-array-acesso')
+
+const numeros = [1, 2, 3, 4, 5]
+
+exemploArrayAcessoDiv.innerHTML = `
+    <h2>Exemplo de aceso aos elementos do array</h2>
+    <p>Primeiro elemento: ${numeros[0]}</p>
+    <p>Segundo elemento: ${numeros[1]}</p>
+    <p>Terceiro elemento: ${numeros[2]}</p>
+    <p>Quarto elemento: ${numeros[3]}</p>
+    <p>Quinto elemento: ${numeros[4]}</p>
+    `
