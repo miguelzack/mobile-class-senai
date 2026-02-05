9cea436b904cd6a3ffa805b3c462df2e9615f598
miguelzackk <mzsilvaw@gmail.com>
2026-02-05 09:59:27 -0300

    testing for in class-two

--- class-two/js/index.js
+++ class-two/js/index.js
@@ -12,11 +12,28 @@ const exemploArrayAcessoDiv = document.getElementById('exemplo-array-acesso')
 
 const numeros = [1, 2, 3, 4, 5]
 
-exemploArrayAcessoDiv.innerHTML = `
-    <h2>Exemplo de aceso aos elementos do array</h2>
-    <p>Primeiro elemento: ${numeros[0]}</p>
-    <p>Segundo elemento: ${numeros[1]}</p>
-    <p>Terceiro elemento: ${numeros[2]}</p>
-    <p>Quarto elemento: ${numeros[3]}</p>
-    <p>Quinto elemento: ${numeros[4]}</p>
-    `
+// exemploArrayAcessoDiv.innerHTML = `
+//     <h2>Exemplo de aceso aos elementos do array</h2>
+//     <p>Primeiro elemento: ${numeros[0]}</p>
+//     <p>Segundo elemento: ${numeros[1]}</p>
+//     <p>Terceiro elemento: ${numeros[2]}</p>
+//     <p>Quarto elemento: ${numeros[3]}</p>
+//     <p>Quinto elemento: ${numeros[4]}</p>
+//     `
+
+numeros.forEach((numeros) => console.log(numeros))
+
+
+
+
+const exemploArrayModificadoDiv = document.getElementById('exemplo-array-modificado')
+
+const cores = ["azul", "amarelo", "vermelho"]
+
+cores[0] = "preto"
+
+
+exemploArrayModificadoDiv.innerHTML = `
+<h2>Exemplo de array modificado</h2>
+<p>Array: [${cores}]</p>
+`
