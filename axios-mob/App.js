ba618cd0c05337773aed44793d61fa8831975e9f
miguelzackk <mzsilvaw@gmail.com>
2026-04-23 10:29:01 -0300

    feat: finish 'axios-mob'

---
+++ axios-mob/App.js
@@ -0,0 +1,60 @@
+import api from './src/services/api'
+import {globalStyles} from "./src/styles/globalStyles";
+import {useEffect, useState} from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ScrollView, Text, TextInput, View, Image} from "react-native";
+
+export default function App() {
+    const [filmes, setFilmes] = useState([])
+    const [pesquisa, setPesquisa] = useState("")
+
+    useEffect(() => {
+        async function carregarFilmes() {
+            if (pesquisa.trim() !== "") {
+                try {
+                    const response = await api.get(pesquisa.replace(" ", "%20"))
+                    setFilmes(response.data)
+                } catch (error) {
+                    console.error("Erro na requisição" + error)
+                }
+            } else {
+                setFilmes([])
+            }
+        }
+
+        carregarFilmes()
+    }, [pesquisa])
+
+
+    const handlePesquisa = (texto) => {
+        setPesquisa(texto)
+    }
+
+    return (<SafeAreaView style={globalStyles.container}>
+        <TextInput style={globalStyles.input} placeholder="Digite o nome do filme" value={pesquisa}
+                   onChangeText={handlePesquisa}/>
+        <Text style={globalStyles.titulo}>Resultado(s) da pesquisa</Text>
+        <ScrollView>
+          {filmes.map((filme) => (
+              <View key={filme.show.id} style={globalStyles.card}>
+                {filme.show.image && (
+                    <Image
+                        source={{ uri: filme.show.image.medium }}
+                        style={globalStyles.imagem}
+                        resizeMode="cover"
+                    />
+                )}
+
+                <View style={globalStyles.infoContainer}>
+                  <Text style={globalStyles.tituloFilme}>
+                    {filme.show.name}
+                  </Text>
+                  <Text style={globalStyles.url}>
+                    {filme.show.url}
+                  </Text>
+                </View>
+              </View>
+          ))}
+        </ScrollView>
+    </SafeAreaView>)
+}
