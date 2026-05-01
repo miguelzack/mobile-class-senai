c41816b683a3dc11badd313847073642022b26a1
miguelzackk <mzsilvaw@gmail.com>
2026-05-01 15:14:24 -0300

    feat: finish header in 'pokemon-api'

--- pokemon-api/App.js
+++ pokemon-api/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Linking} from 'react-native';
 import {styles} from "./appStyles";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import LogoGitHub from "./src/assets/icons/logoGitHub";
+import {useGlobalFonts} from "./src/styles/global";
 
 export default function App() {
 
-    return (<View>
-        <Text style={styles.text}>Minecraft Style</Text>
-    </View>);
+    const fontsLoaded = useGlobalFonts();
+
+    const openGitHub = () => {
+        Linking.openURL('https://github.com/miguelzack');
+    };
+
+    return (<SafeAreaProvider>
+
+        <SafeAreaView>
+            <View style={styles.header}>
+                <TouchableOpacity style={styles.link} onPress={openGitHub}>
+                    <Text style={styles.textLink}>MiguelZack</Text>
+                    <LogoGitHub/>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+
+    </SafeAreaProvider>);
 }
--- pokemon-api/appStyles.js
+++ pokemon-api/appStyles.js
@@ -1,8 +1,24 @@
-import { StyleSheet } from 'react-native';
-import { FONTS, COLORS } from './src/styles/global';
+import {StyleSheet} from 'react-native';
+import {FONTS, COLORS} from './src/styles/global';
 
 export const styles = StyleSheet.create({
-    text: {
-        fontFamily: FONTS.minecraft
+    header: {
+        width: "100%",
+        backgroundColor: COLORS.textDark,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        flexDirection: "row",
+    }, link: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    }, textLink: {
+        color: COLORS.text,
+        fontSize: 16,
+        fontFamily: FONTS.minecraft,
+        marginRight: 16,
+        top: 3
     }
 });
